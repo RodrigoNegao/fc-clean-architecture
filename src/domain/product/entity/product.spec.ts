87481e2c6d6140187d4c22829c7a9592ba303140
rodrigo.dacunha <rodrigo.dacunha@apoio.agro.gov.br>
2024-05-06 10:27:32 -0300

    test: add test

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -19,6 +19,12 @@ describe("Product unit tests", () => {
     }).toThrowError("Price must be greater than zero");
   });
 
+  it("should throw two errors when name is empty and price is less is null", () => {
+    expect(() => {
+      const product = new Product("123", "", null);
+    }).toThrowError("product: Name is required,product: Price must be greater than zero");
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
